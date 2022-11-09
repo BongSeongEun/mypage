3ffb797562c439b9ff8488344dc479f860b48223
seongeunbong <bongfam2@naver.com>
2022-11-09 13:17:26 +0900

    라우터 적용

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Welcome from './Pages/Welcome';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <Routes>
+          {/* ---- 메인페이지 ---- */}
+          <Route path='/' element={<Welcome />} />
+        </Routes>
+      </Router>
+      {/* <Link to='/' style={{ color: 'inherit', textDecoration: 'inherit' }}>
+        아이디 찾기&emsp;&emsp;
+      </Link> */}
     </div>
   );
 }
