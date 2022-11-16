4a9a1528d363cfa95f3d26b06a3cdcffc294e570
seongeunbong <bongfam2@naver.com>
2022-11-16 15:21:36 +0900

    TodoList 구현

---
+++ src/Components/TodoList.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import React from 'react';
+import styled from 'styled-components';
+
+const MainWrap = styled.div`
+  flex: 1;
+  padding: 1.5rem, 2rem;
+  padding-bottom: 3rem;
+  overflow-y: auto;
+`;
+function TodoList() {
+  return <MainWrap>TodoList</MainWrap>;
+}
+export default TodoList;
