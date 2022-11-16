e4fe6a32ae4bf5a339079fd7327d7a9e1f3cef2f
seongeunbong <bongfam2@naver.com>
2022-11-16 19:07:04 +0900

    개발 환경 설정

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -2,12 +2,15 @@
 import React from 'react';
 import styled from 'styled-components';
 import TodoItem from './TodoItem';
+import { useState, useEffect } from 'react';
+
 const MainWrap = styled.div`
   flex: 1;
   padding: 1.5rem, 2rem;
   padding-bottom: 3rem;
   overflow-y: auto;
 `;
+
 function TodoList() {
   return (
     <MainWrap>
