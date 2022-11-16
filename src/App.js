4109e80f6df242debc305a0d9b4b9068f056eb50
seongeunbong <bongfam2@naver.com>
2022-11-16 15:16:25 +0900

    컴포넌트 적용

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+/* eslint-disable */
 import React from 'react';
 import styled from 'styled-components';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Welcome from './Pages/Welcome';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import { createGlobalStyle } from 'styled-components';
 
 function App() {
   return (
--- src/Pages/Welcome.js
+++ src/Pages/Welcome.js
@@ -1,5 +1,27 @@
+/* eslint-disable */
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import styled from 'styled-components';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import TodoTemplate from '../Components/TodoTemplate';
+import TodoHead from '../Components/TodoHead';
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    background: #e9ecef;
+  }
+`;
+
 function Welcome() {
-  return <div>Reminder Project</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <TodoTemplate>
+        <TodoHead />
+      </TodoTemplate>
+    </>
+  );
 }
 export default Welcome;
